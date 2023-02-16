2b16f88bb8f74323fa9f4f44b62bc69f6bbe23e7
david.vila <davilasanvilar@gmail.com>
2023-02-16 10:53:06 +0100

    CoolCheckBox finished

--- src/components/atom/CoolCheckbox.tsx
+++ src/components/atom/CoolCheckbox.tsx
@@ -1,11 +1,12 @@
 import React, { MouseEventHandler } from 'react';
 import styled from 'styled-components';
-import { SelectOption, SizeEnum } from '../../types/types';
+import { IconTypeEnum, SelectOption, SizeEnum } from '../../types/types';
 import { SelectBase } from '../bases/SelectBase';
 import { device } from '../../StyledTheme';
 import { TextInputBase } from '../bases/TextInputBase';
 import { CheckboxBase } from '../bases/CheckboxBase';
 import { BsCheck } from 'react-icons/bs';
+import { CoolIcon } from './CoolIcon';
 
 
 interface MainSizeProps {
@@ -14,16 +15,16 @@ interface MainSizeProps {
 
 interface SizeProps {
     width: number //UNIT: px
-    fontSize: string
 }
 
 interface AllProps extends SizeProps {
-    isDark?: boolean
     children?: JSX.Element | JSX.Element[]
+    disabled?: boolean
 }
 
 const MainBox = styled.div<MainSizeProps>`
     display: flex;
+    align-items: center;
     width: ${props => `${props.totalWidth ? props.totalWidth : undefined}`};
 `;
 
@@ -33,63 +34,67 @@ const HiddenCheckbox = styled(CheckboxBase)`
 
 const CustomCheckbox = styled.div<AllProps>`
     position: relative;
+    border-radius: 12px;
     width: ${props => `${props.width}px`};
+    height: ${props => `${props.width}px`};
     transition: background .2s;
+    border: 2px solid ${props => props.theme.color.main.l3};
+
     display: flex;
     align-items: center;
-    
     &:hover {
-        transition: background .2s;
-        background-color: ${props => props.theme.color.hoverInputLight} ;
+        border: ${props => props.disabled ? undefined : `2px solid ${props.theme.color.main.d1}`};
     }
+    
+    &:focus {
+        border: 2px solid ${props => props.theme.color.highlight.n} !important;
+        transition: border .2s;
+    }  
 `
 
 const CoolStyledCheckbox = styled.div<AllProps>`
     padding: 5px;
-    font-size: ${props => props.fontSize};
+    font-size: 1rem;
     width: ${props => `${props.width}px`};
     height: ${props => `${props.width}px`};
-    background: ${props => props.isDark ? props.theme.color.mainColor : props.theme.color.lightBackground};
-    color: ${props => props.isDark ? props.theme.color.lightFont : props.theme.color.darkFont};
-    border: none;
     transition: border .2s;
     box-sizing: border-box;
     display: flex;
     align-items: center;
-    justify-content: space-around;
+    justify-content: center;
     border: 3px solid transparent;
+    background-color: ${props => props.disabled ? props.theme.color.main.lowOp : undefined};
+    border-radius: 50%;
     outline:none;
-    @media ${device.desktopL} { 
-        width: ${props => `${1.2 * props.width}px`};
-    }
     & div {
         overflow: hidden;
 
         width: 80%;
     }
-    &:focus {
-        border: 3px solid ${props => props.theme.color.highlightColor};
-        transition: border .2s;
-    }  
 `;
 
 const CoolStyledCheck = styled.div<AllProps>`
     z-index: 3;
     position: absolute;
     display: flex;
+    width: 100%;    
     justify-content: center;
+    padding: 0;
+    margin:0;
     align-items: center;
     & svg {
-        font-size: 2rem;
+        font-size: 1rem;
         width: 100%;
         height: 100%;
-        color: ${props => props.isDark ? props.theme.color.lightFont : props.theme.color.mainColor};
+        color: ${props => props.theme.color.main.n};
         box-sizing: border-box;
     }
 `
 
-const CoolStyledLabel = styled.label`
+const CoolStyledLabel = styled.label<AllProps>`
     margin-left: 10px;
+    color: ${props => props.theme.color.main.d7};
+    opacity: ${props => props.disabled ? .6 : undefined };
 `
 
 
@@ -97,36 +102,45 @@ const CoolStyledLabel = styled.label`
 const getSize = (size?: SizeEnum): SizeProps => {
     switch (size) {
         case SizeEnum.L:
-            return { width: 20, fontSize: '2rem' }
+            return { width: 20 }
         case SizeEnum.M:
-            return { width: 20, fontSize: '2rem' }
+            return { width: 20 }
         case SizeEnum.S:
-            return { width: 20, fontSize: '2rem' }
+            return { width: 20 }
         case SizeEnum.XS:
-            return { width: 20, fontSize: '2rem' }
+            return { width: 15 }
         default:
-            return { width: 20, fontSize: '2rem' }
+            return { width: 15 }
     }
 }
 
-export function CoolCheckbox({ id, value, setValue, size, isDark, label, width }: {
-    id?: string, value: boolean, setValue: React.Dispatch<React.SetStateAction<boolean>>, size?: SizeEnum, isDark?: boolean, label: string, width?: string
+export function CoolCheckbox({ id, value, setValue, size, label, children, disabled }: {
+    id?: string, value: boolean, setValue: React.Dispatch<React.SetStateAction<boolean>>, size?: SizeEnum, label?: string,
+    children?: JSX.Element | JSX.Element[], disabled?: boolean
 }) {
 
     const sizeInfo = getSize(size)
 
     const onClick = () => {
-        setValue((old) => !old)
+        if (!disabled) {
+            setValue((old) => !old)
+        }
     }
 
     return (
-        <MainBox totalWidth={width} onClick={onClick}>
+        <MainBox onClick={onClick}>
             <HiddenCheckbox setValue={setValue} value={value} />
-            <CustomCheckbox width={sizeInfo.width} fontSize={sizeInfo.fontSize}>
-                <CoolStyledCheckbox width={sizeInfo.width} fontSize={sizeInfo.fontSize} isDark={isDark} />
-                {value ? <CoolStyledCheck fontSize={sizeInfo.fontSize} width={sizeInfo.width} isDark={isDark}><BsCheck /></CoolStyledCheck> : <></>}
+            <CustomCheckbox width={sizeInfo.width} disabled={disabled}>
+                <CoolStyledCheckbox width={sizeInfo.width} disabled={disabled} />
+                {value ? <CoolStyledCheck width={sizeInfo.width}>
+                    <CoolIcon type={IconTypeEnum.CHECK} />
+                </CoolStyledCheck>
+                    : <></>}
             </CustomCheckbox>
-            <CoolStyledLabel>{label}</CoolStyledLabel>
+            {label ? <CoolStyledLabel width={sizeInfo.width} disabled={disabled} >{<>{label}</>}</CoolStyledLabel>
+                :
+                <CoolStyledLabel width={sizeInfo.width} >{children}</CoolStyledLabel>
+            }
         </MainBox>
     )
 }
