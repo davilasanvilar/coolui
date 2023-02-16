1a2ec64c9895dedcd51d0f930ead608b4355c477
david.vila <davilasanvilar@gmail.com>
2023-02-16 12:29:04 +0100

    CoolSelect finished

--- src/components/atom/CoolSelect.tsx
+++ src/components/atom/CoolSelect.tsx
@@ -1,85 +1,51 @@
-import React, { MouseEventHandler } from 'react';
 import styled from 'styled-components';
 import { SelectOption, SizeEnum } from '../../types/types';
 import { SelectBase } from './../bases/SelectBase';
-import { device } from '../../StyledTheme';
 
 
-interface SizeProps {
-    width: number //UNIT: px
-    height: number //UNIT: px
-    fontSize: string
-}
-
-interface AllProps extends SizeProps {
-    isDark?: boolean
+interface AllProps {
+    disabled?: boolean
 }
 
 
 const CoolStyledSelect = styled(SelectBase) <AllProps>`
-    padding: 5px;
-    font-size: ${props => props.fontSize};
-    width: ${props => `${props.width}px`};
-    height: ${props => `${props.height}px`};
-    background: ${props => props.isDark ? props.theme.color.mainColor : props.theme.color.lightBackground};
-    color: ${props => props.isDark ? props.theme.color.lightFont : props.theme.color.darkFont};
+    padding: 14px 24px;
+    border-radius: 12px;
+    font-size: ${props => props.theme.fontSize.regularText};
+    width: 100%;
+    min-height: 52px;
+    height: 52px;
+    background-color: ${props => props.disabled ? props.theme.color.main.l4 : props.theme.color.main.l6} ;
+    color: ${props => props.theme.color.main.d7};
     border: none;
-    transition: background .2s;
     box-sizing: border-box;
     display: flex;
     align-items: center;
     justify-content: space-around;
     overflow: hidden;
-    border: 3px solid transparent;
+    border: 2px solid transparent;
     outline:none;
-    @media ${device.desktopL} { 
-        width: ${props => `${1.2 * props.width}px`};
-        height: ${props => `${1.2 * props.height}px`};
-    }
-    & svg {
-        font-size: 2rem;
-        width: 20%;
-        margin-right: 5%;
-        box-sizing: border-box;
-    }
-    & div {
-        overflow: hidden;
-
-        width: 80%;
-    }
     &:focus {
-        border: 3px solid ${props => props.theme.color.highlightColor};
+        border: 2px solid ${props => props.disabled ? undefined : props.theme.color.highlight.n};
+        background-color: ${props => props.disabled ? undefined : props.theme.color.main.l5} ;
         transition: border .2s;
     }
+    transition: background .2s;
+    
     &:hover {
         transition: background .2s;
-        background: ${props => props.isDark ? props.theme.color.hoverInputDark : props.theme.color.hoverInputLight};
-    }    
+        background-color: ${props => props.disabled ? undefined : props.theme.color.main.l5} ;
+    }
+    
 `;
 
-
-export function CoolSelect({ id, options, value, setValue, size, isDark }: {
-    id: string, options: SelectOption[], value: string, setValue: (value: string) => void, size?: SizeEnum, isDark?: boolean
+export function CoolSelect({ id, options, value, setValue, noEmpty, disabled }: {
+    id: string, options: SelectOption[], value: string, setValue: (value: string) => void,
+    noEmpty?: boolean, disabled?: boolean
 }) {
 
-    const getSize = (): SizeProps => {
-        switch (size) {
-            case SizeEnum.L:
-                return { width: 300, height: 40, fontSize: '1rem' }
-            case SizeEnum.M:
-                return { width: 250, height: 40, fontSize: '1rem' }
-            case SizeEnum.S:
-                return { width: 200, height: 40, fontSize: '1rem' }
-            case SizeEnum.XS:
-                return { width: 100, height: 40, fontSize: '1rem' }
-            default:
-                return { width: 250, height: 40, fontSize: '1rem' }
-        }
-    }
-
-    const sizeInfo = getSize()
-
     return (
-        <CoolStyledSelect height={sizeInfo.height} width={sizeInfo.width} fontSize={sizeInfo.fontSize} options={options} setValue={setValue} value={value} isDark={isDark} />
+        <CoolStyledSelect options={options} setValue={setValue} value={value}
+            noEmpty={noEmpty} disabled={disabled} />
     )
 }
--- src/components/bases/SelectBase.tsx
+++ src/components/bases/SelectBase.tsx
@@ -1,17 +1,19 @@
-import React, { MouseEventHandler } from 'react';
+import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
-import { ButtonTypeEnum, SelectOption } from '../../types/types';
+import { SelectOption } from '../../types/types';
 
 const ButtonStyled = styled.button`
 `;
 
-export function SelectBase({ options, value, setValue, className }: {
-    className?: string, options: SelectOption[], value: string, setValue: (value: string) => void
+export function SelectBase({ options, value, setValue, className, noEmpty, disabled }: {
+    className?: string, options: SelectOption[], value: string, setValue: (value: string) => void, 
+    noEmpty?: boolean, disabled?: boolean
 }) {
+    const { t } = useTranslation()
 
     return (
-        <select className={className} value={value} onChange={(e) => setValue(e.target.value)}>
-            <option key={'undefined_key'} value={''}>{'Seleccione una opción'}</option>
+        <select className={className} disabled={disabled} value={value} onChange={(e) => setValue(e.target.value)}>
+            {!noEmpty ? <option key={'undefined_key'} value={''}>{t('label.selectOption')}</option> : undefined}
             {options.map((option) =>
                 <option key={option.value} value={option.value}>{option.label}</option>)
             }
